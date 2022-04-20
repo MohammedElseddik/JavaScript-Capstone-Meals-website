126d87238863b6a90b04e9f60dae653abda46a5a
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-20 09:34:56 +0100

    Fix linter issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
+  entry: './src/index.js',
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  devServer: {
+    static: './dist',
+  },
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-
-    devServer: {
-        static: './dist',
-    },
-
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  },
 };
