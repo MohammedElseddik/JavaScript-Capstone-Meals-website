cfa369abf833e4c92e84d66f3a750fb9221da569
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-20 18:40:23 +0200

    Add the fetchMeals function fetch meals from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import displayMeals from './modules/displayMeals.js';
+
+const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=c';
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayMeals(URL);
+})
---
+++ src/modules/fetchMeals.js
@@ -0,0 +1,12 @@
+
+const fetchMeals = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw Error(error);
+    }
+}
+
+export default fetchMeals;
