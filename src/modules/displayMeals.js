8abc1a2ef59588cec01c25cc0e89307262d0f2fa
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-21 21:58:46 +0200

    Fix linter issues

--- src/modules/displayMeals.js
+++ src/modules/displayMeals.js
@@ -4,7 +4,7 @@ import showAndHidePopup from "./popup.js";
 const displayMeals = async (url) => {
   // fetch data
   const data = await fetchMeals(url);
-
+  // get data
   // show data
   renderMeals(data);
   showAndHidePopup()
