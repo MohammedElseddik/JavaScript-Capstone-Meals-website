c0364f358e205c6030c9c6cce71e376b8ce987ce
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-22 14:19:02 +0200

    Add ingredients to the popup

--- src/modules/displayMeals.js
+++ src/modules/displayMeals.js
@@ -1,13 +1,14 @@
 import fetchMeals from "./fetchMeals.js";
 import renderMeals from "./renderMeals.js";
-import showAndHidePopup from "./popup.js";
+import selectCard from "./popup.js";
 const displayMeals = async (url) => {
   // fetch data
   const data = await fetchMeals(url);
+  console.log(data);
   // get data
   // show data
   renderMeals(data);
-  showAndHidePopup()
+  selectCard(data);
 };
 
 export default displayMeals;
