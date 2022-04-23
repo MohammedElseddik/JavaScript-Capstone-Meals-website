d5655d4f49af86005cfa878c00b90ba22f44d20d
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-23 13:13:43 +0100

    Remove console.log

--- src/modules/displayMeals.js
+++ src/modules/displayMeals.js
@@ -6,7 +6,6 @@ import { getLikesNumber, fetchLikes } from './likes.js';
 const involvementUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/CUZJYbcXhLNNl7JTkeKF/likes';
 
 const displayMeals = async (mealUrl) => {
-  console.log(mealUrl);
   const mealData = await fetchMeals(mealUrl);
   const likes = await fetchLikes(involvementUrl);
   renderMeals(mealData, likes);
